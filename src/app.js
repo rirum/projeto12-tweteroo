da614c2d6937bc3ebbe0b28218ad8c0f9776dc99
camille <c.miyashita@gmail.com>
2023-01-06 22:02:31 -0300

    sign up ok

--- src/app.js
+++ src/app.js
@@ -2,15 +2,52 @@ import express from "express";
 import cors from "cors";
 
 const server = express()
+server.use(express.json())
 server.use(cors())
+
+
+const PORT = 5000;
+const users = []; // array user
+const tweets = []; // array tweets
 
 // req -> recebe infos
 // res -> envia infos
 
-server.get("/tweets", (req, res) => {
-  res.send("teste") 
+server.post("/sign-up", (req, res) => {
+  const {username, avatar} = req.body; // receber pelo body da request o username e o avatar
+  const userData = {username, avatar};
+  users.push(userData); // salvar o usuário na array de usuarios
+  console.log(users); 
+  res.status(201).send("OK"); //mensagem de ok
 })
 
-server.listen(5000, () => {
-    console.log('testando!')
+server.post("/tweets", (req, res) => {
+  const { username } = req.headers;
+  console.log(req.headers);
+  const { tweet } = req.body;
+  
+  const user = users.find(user => user.username === username);
+  console.log(user)
+
+  if(!user) {
+    return res.status(401).send("UNAUTHORIZED");
+  }
+
+  tweets.push({ username, tweet });
+
+  res.status(201).send("OK") 
+})
+
+server.get("/tweets", (req, res) => {
+  if (tweets.length > 10) {
+    const lastMsg = tweets.slice(-10);
+    res.send(lastMsg);
+  } else {
+    res.send(tweets);
+  }
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
 })
